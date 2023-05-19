c21846d4403ec14af2693b7d00318d4946242599
Saketh Rajesh <sakethraj122@gmail.com>
2023-05-19 09:48:43 -0700

    view comments unauthenticated

--- app/gallery/comments.js
+++ app/gallery/comments.js
@@ -63,16 +63,18 @@ export default function comments({title, id}) {
             content: content
         },
     ])
-    
-      
+}
 
-
+async function handleOAuthLogin(provider) {    
+    const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: provider,
+        redirectTo: '/gallery'
+    })
+  
+    if (error) console.log('Error: ', error.message)
   }
 
   return (
-    <>
-        {
-            session ? (
                 <>
                 <div className="">
                     <button
@@ -80,7 +82,7 @@ export default function comments({title, id}) {
                     onClick={openModal}
                     className="w-full rounded bg-black bg-opacity-20 px-4 py-2 text-sm  font-serif text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
                     >
-                    Open Comments
+                    View {comments.length} Comments
                     </button>
                 </div>
             
@@ -134,7 +136,29 @@ export default function comments({title, id}) {
                                         </div>
                                     )
                                 }
-                                <Form onSubmit={submit}/>
+                                {
+                                    session ? (
+                                        <Form onSubmit={submit}/>
+                                    ) : (
+                                        
+                                        <button
+                                            onClick={() => handleOAuthLogin('google')}
+                                            type="button"
+                                            className="py-3 flex flex-row items-center justify-center focus:ring-blue-500 focus:border-blue-500 w-full border-neutral-300 rounded-md bg-gray-100 dark:bg-neutral-800 text-neutral-900 dark:text-neutral-100"
+                                            
+                                        >
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 32 32"
+                                                className="w-4 h-4 fill-current"
+                                            >
+                                                <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
+                                            </svg>
+                                            <p className="text-xs font-serif ml-2">sign in with Google to leave a comment</p>
+                                        </button>
+
+                                    )
+                                }
 
                             </Dialog.Panel>
                         </Transition.Child>
@@ -142,19 +166,6 @@ export default function comments({title, id}) {
                     </div>
                     </Dialog>
                 </Transition>
-                </>
-            ) : (
-                <div className="">
-                    <button
-                    type="button"
-                    onClick={() => {}}
-                    className="w-full rounded bg-black bg-opacity-20 px-4 py-2 text-sm font-serif text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
-                    >
-                    Sign in to View Comments
-                    </button>
-                </div>
-            )
-        }
     </>
   )
 }
